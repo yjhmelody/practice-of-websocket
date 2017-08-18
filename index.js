957e2c2f67618b9e5f071181e168d3466efb9f79
yjhmelody <465402634@qq.com>
2017-08-18 19:50:04 +0800

    update

--- index.js
+++ index.js
@@ -2,22 +2,38 @@ const WebSocket = require('ws')
 
 const wss = new WebSocket.Server({port:8080})
 
+let count = 0
 wss.on('connection', (ws, req) => {
     let ip = req.connection.remoteAddress
-    console.log('remote address', ip)
-    // console.log('remote address', req.headers['x-forward-for'])
+    // console.log('remote address ' + ip)
+    count++
+    ws.nickname = 'user ' + count
+
+    broadcast(ws.nickname + ' comes in')
     ws.on('message', (msg)=>{
-        console.log(new Date + ip + ' say to you:' + msg)
+        // console.log('Received' + msg)
+        broadcast(msg)
+    })
+    ws.on('message', (msg)=>{
+        // console.log(new Date + ip + ' say to you:' + msg)
     })
     ws.on('close', (code, msg) =>{
         console.log(ip + ' has left', code, msg)
+        broadcast(ws.nickname + ' left')
     })
 })
 
 wss.on('headers', (data) => {
-    console.log('headers', data)
+    // console.log('headers', data)
 })
 
 wss.on('error', (err) => {
     console.log(new Date, err)
 })
+
+
+function broadcast(msg){
+    wss.clients.forEach(function(client){
+        client.send(msg)
+    })
+}
